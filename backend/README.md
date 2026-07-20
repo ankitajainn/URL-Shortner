b2da6104c1a901d348028c51e481a3f3fbd8fd4d
Ankita <jainankitjain@gmail.com>
2026-07-20 10:28:28 +0530

    Fix: Add CORS middleware for frontend connection

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+﻿# URL-Shortner
+
+1)To open drizzle ORM
+<br>
+    pnpm db:studio 
+  <br>
+  <br>
+  <br>
+2)To run the server
+<br>
+    pnpm dev
---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import 'dotenv/config'
+import {authenticationMiddleware} from './middlewares/auth.middleware.js'
+
+import userRouter from './routes/user.routes.js'
+import urlRouter from './routes/url.routes.js'
+
+const app=express();
+const PORT=process.env.PORT ?? 8000;
+
+
+// 2. Enable CORS for your frontend
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+
+app.use(express.json());
+app.use(authenticationMiddleware)
+
+app.get('/',(req,res)=>{
+    return res.json({status:'Server is up here'});
+});
+
+app.use(urlRouter);
+app.use('/user',userRouter);
+
+
+app.listen(PORT,()=>{
+    console.log(`Server in on ${PORT}`);
+})
